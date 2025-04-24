4ecd93461f40e848f521c54ce9af4b72b38b65e0
puneeeeeeet <puneetchhabra1607@gmail.com>
2025-04-24 16:50:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-dvh bg-[#E82677] relative">
+      <div className=" inset-0 bg-white mt-14 mx-20 rounded-md absolute ">
+        <div className="flex justify-center items-center flex-col">
+          <div className="mt-14 text-2xl">Events</div>
+          <div className="flex items-center justify-center text-sm w-2/5 text-center">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur.</div>
+
+        </div>
+      </div>
     </div>
   );
 }
